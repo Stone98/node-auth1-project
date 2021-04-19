70b70c7acab1876595feb3ff0d709c520649f355
Stone98 <stone18003@gmail.com>
2021-04-19 09:13:33 -0700

    auth router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,14 @@
+const router = require("express").Router();
+const User = require("../users/users-model");
+const bcrypt = require("bcryptjs");
+
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
-
+const {
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+} = require("./auth-middleware");
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,6 +33,23 @@
   }
  */
 
+router.post(
+  "/register",
+  checkUsernameFree,
+  checkPasswordLength,
+  async (req, res, next) => {
+    try {
+      const hash = bcrypt.hashSync(req.body.password, 8);
+      const newUser = await User.add({
+        username: req.body.username,
+        password: hash,
+      });
+      res.json(newUser);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -42,6 +67,22 @@
   }
  */
 
+router.post("/login", checkUsernameExists, async (req, res, next) => {
+  try {
+    const verified = bcrypt.compareSync(
+      req.body.password,
+      req.userData.password
+    );
+    if (verified) {
+      req.session.user = req.userData;
+      res.json({ message: `Welcome ${req.userData.username}!` });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 /**
   3 [GET] /api/auth/logout
@@ -59,5 +100,29 @@
   }
  */
 
- 
+router.get("/logout", (req, res, next) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.json("Can't log out");
+      } else {
+        res.status(200).json({ message: "logged out" });
+      }
+    });
+  } else {
+    res.status(500).json({ message: "no session" });
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(500).json({
+    sageAdvice: "Finding the real error is 90% of the bug fix",
+    error: err.message,
+    stack: err.stack,
+  });
+});
+
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -21,11 +26,15 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
